1926e193e52f5188caf4bc249b0b210000fd5051
ColtMoney <masterflomastermaster@gmail.com>
2019-06-23 22:07:55 +0300

    Work Commit

---
+++ aviasales/precache-manifest.24d9f5cda242cd8783a7850972b4a3ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcd8bab6cb68e812c351078a251b4acb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c014ab2562619a96452b",
+    "url": "./static/css/main.4c3dd870.chunk.css"
+  },
+  {
+    "revision": "212358c63c147353dfb1",
+    "url": "./static/js/2.fe0a153c.chunk.js"
+  },
+  {
+    "revision": "c014ab2562619a96452b",
+    "url": "./static/js/main.ce790d6d.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
--- aviasales/service-worker.js
+++ aviasales/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e98416b776b5747515e7a11760dbc21d.js"
+  "./precache-manifest.24d9f5cda242cd8783a7850972b4a3ad.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
