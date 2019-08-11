bf4f0ac554c126282716bfa81a597a74179c2dda
ColtMoney <masterflomastermaster@gmail.com>
2019-08-11 22:58:23 +0300

    Work Commit

---
+++ configurator/precache-manifest.2eb0829691dacaae51757a9f66edb94b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84c2ce3f9a5969e7f60b31c35c40b082",
+    "url": "./index.html"
+  },
+  {
+    "revision": "732143d8fab2caacec5b",
+    "url": "./static/css/main.1cae8c5b.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "./static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "732143d8fab2caacec5b",
+    "url": "./static/js/main.6d8ff7f2.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
