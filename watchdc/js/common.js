9173adb553a295d4a333ed2d395e7091b5d251b5
ColtMoney <masterflomastermaster@gmail.com>
2024-11-05 10:11:18 +0200

    watchdc fixes

--- watchdc/js/common.js
+++ watchdc/js/common.js
@@ -223,4 +223,75 @@ $(function () {
 
 	}
 
+	$('.accordion_plus_btn').click(function(){
+		let popupTarget = $(this).data('popup-target');
+
+		$('.popup_wrpr').removeClass('active');
+		$('.popup_wrpr.'+popupTarget).addClass('active');
+
+		return false;
+	});
+
+	$('.close_popup_btn').click(function(){
+		$('.popup_wrpr').removeClass('active');
+
+		return false;
+	});
+
+	$('.bell-info a').click(function(){
+		$('.alerts_notifications').toggleClass('active');
+
+		return false;
+	});
+
+	$('.alerts_notifications_close').click(function(){
+		$('.alerts_notifications').removeClass('active');
+
+		return false;
+	});
+
 });
+
+$(document).ready(function () {
+
+	$('ul.tabs li').click(function () {
+			var tab_id = $(this).attr('data-tab');
+
+			$('ul.tabs li').removeClass('current');
+			$('.tabs-content').removeClass('current');
+
+			$(this).addClass('current');
+			$("#" + tab_id).addClass('current');
+	})
+
+	$('.js-tab-trigger').click(function () {
+			var id = $(this).attr('data-tabs'),
+					content = $('.js-tab-content[data-tabs="' + id + '"]');
+
+			$('.js-tab-trigger.active').removeClass('active');
+			$(this).addClass('active');
+
+			$('.js-tab-content.active').removeClass('active');
+			content.addClass('active');
+	});
+
+	$('.download_link').on('click', function () {
+			$('.download_file_popup').addClass('active');
+			return false;
+	});
+
+	$('.top-nav .menu_mobile').on('click', function () {
+			$('.left-sidebar .sidebar-navigation').toggleClass('on_menu');
+			return false;
+	});
+
+	var width = $('.g-recaptcha').parent().width();
+	if (width < 302) {
+			var scale = width / 302;
+			$('.g-recaptcha').css('transform', 'scale(' + scale + ')');
+			$('.g-recaptcha').css('-webkit-transform', 'scale(' + scale + ')');
+			$('.g-recaptcha').css('transform-origin', '0 0');
+			$('.g-recaptcha').css('-webkit-transform-origin', '0 0');
+	}
+
+})
