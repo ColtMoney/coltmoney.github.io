27ae2cc2a28e3417a9539272929ff95bbf063de5
ColtMoney <masterflomastermaster@gmail.com>
2024-11-05 10:11:18 +0200

    watchdc fixes

--- watchdc/inc/footer.php
+++
@@ -1,9 +0,0 @@
-<script src="assets/jquery/jquery.min.js"></script>
-<script src="assets/fabric.min.js"></script>
-<script src="js/common.js"></script>
-<script src="js/common_anton.js"></script>
-<script src="js/common_artur.js"></script>
-
-</body>
-
-</html>
--- watchdc/inc/tabs-top.php
+++
@@ -1,9 +0,0 @@
-<?php if ( $tabs ) : ?>
-    <ul class="tabs">
-		<?php foreach ( $tabs as $key => $value ): ?>
-            <li class="tab-link <?php if ( $key == 0 ) {
-				echo 'current';
-			} ?>" data-tab="tab-<?php echo $key; ?>"><?php echo $value; ?></li>
-		<?php endforeach; ?>
-    </ul>
-<?php endif; ?>
